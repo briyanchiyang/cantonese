af7f1541b7bd8ae0a5335f6d8a3f1944af6f134a
briyanchiyang <brian.chiang.25@dartmouth.edu>
2023-09-08 18:13:54 -0400

    major README changes

--- README.md
+++ README.md
@@ -2,8 +2,47 @@
 
 #### Training a model using Meta's Wav2Vec2 to recognize mispronunciations in Cantonese
 
+## Motivation
 My Cantonese is worse than it should be for someone who grew up in Hong Kong. I'm okay at conversing,
 but have always wondered if there was a way to use coding to improve my pronunciation. It turns out there
 <em>is</em> a way — I've trained a model to recognize phoneme-level mispronunciations in Cantonese. Although
 it's obviously imperfect (there are many other factors that affect pronunciation), it's a start. I
 use this as a tool to improve my own pronunciation, and hope others to do the same.
+
+## Installation
+Clone the repo by the following:
+```
+git clone https://github.com/briyanchiyang/cantonese.git
+```
+
+Install the required dependencies. Since many of the packages are quite specific, it's recommended to use a virtual environment to store all of these.
+```
+pip install -r requirements.txt
+```
+
+The webapp is built using Flask in `model.py`, so to run the webapp run the file:
+```
+python model.py
+```
+Finally, access the webapp through the localhost address, which by default should be 127.0.0.1:5000. Copy and paste `http://127.0.0.1:5000` into your url and click enter. Upon viewing the site, click the red button to record "你叫咩名啊?", which is Cantonese for "what is your name?" The results of your recording should display shortly after.
+
+## Methodology
+### 1. Training a model to recognize phonemes
+I built a HuggingFace model that returns an array of Cantonese phonemes when provided with an audio recording. The model is finetuned from a pretrained XLSR-53 Wav2Vec2 model. As outlined in *Unsupervised Cross-lingual Representation Learning for Speech Recognition* by Conneau et al. ([here](https://arxiv.org/pdf/2006.13979.pdf)), this model learns cross-lingual speech representations: in other words, it accurately predicts phonemes in some given test language by being trained on data from multiple languages. This is possible because there are phonemes shared between languages, and training multilingually increases the amount of training data. This can even be used for zero-shot phoneme recognition: in other words, recognizing phonemes from languages the model has never even seen before.
+
+Wav2Vec2 itself is a model that uses contrastive learning to learn latent audio representations -- in other words, it learns how to represent audio in a vector space. More information can be found [here](https://ai.meta.com/blog/wav2vec-20-learning-the-structure-of-speech-from-raw-audio/).
+
+For the project itself, I trained the model on 140 hours of Cantonese (all from Hong Kong to avoid accents) from the Common Voice dataset. After identifyfing 209 phonemes, I fine-tuned the XLSR-53 model to recognize them with a 2.6% phoneme error rate. I uploaded the model to [HuggingFace](https://huggingface.co/briyanchiang/cantoclarity) and call on it with Flask in lines 18 and 19. It's important to note that all of this was done on Google Colab and not locally due to various benefits (e.g. access to GPUs/TPUs, pre-installed ML libraries, etc.).
+
+### 2. Using the model to identify mispronunciations
+With phoneme predictions also comes probabilities associated with those predictions. For each phoneme the model interprets, it gives 209 (actually 213, but I won't go into that) probabilities for each phoneme, each probability indicating whether they are a match. The model deems the correct phoneme to be the one with the highest probability. Using this information, I identify mispronunciations in two fronts:
+
+1. If you do not say the correct phoneme (if the model expects you to say a phoneme and you don't), that is labeled as a mispronunciation and is shown as red.
+
+2. If you do say the correct phoneme, the model calculates its confidence in that phoneme based on the average probabilities of the next five likelist phonemes (shown below as `next_five_avg`). If the phoneme you said has a far greater probability than everything else (shown below as `correct_phoneme`), your pronunciation is probably good. If the probabilities are all close, you probably can work on your pronunciation. In practice, I use the following equation:
+
+$$ accuracy = 1 - (next_five_avg / correct_phoneme) $$
+
+After calculating the accuracies for each phoneme, I then display that information on the webapp.
+
+<!--### 3. Building the frontend-->
---
+++ model.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import flask
+import torch
+
+# Model packages
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor, TrainingArguments
+import numpy as np
+
+# Audio recording packages
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+# Create application
+APP = flask.Flask(__name__)
+
+# Initialize model and processor
+model = Wav2Vec2ForCTC.from_pretrained("briyanchiang/cantoclarity")
+processor = Wav2Vec2Processor.from_pretrained("briyanchiang/cantoclarity")
+print("e")
+
+# All phoneme-related information for the sentence
+class PhonemeWrapper:
+  def __init__(self, phoneme, id, corresponding_id, accuracy):
+    self.phoneme = phoneme
+    self.id = id
+    self.corresponding_id = corresponding_id
+    self.accuracy = accuracy
+
+  def print_all(self):
+    print("Phoneme: ", self.phoneme, "\tId: ", self.id, "\tCorresponding id: ", self.corresponding_id, "\tAccuracy: ", self.accuracy)
+
+
+def probability_analysis(ground_ids, input_ids, logits):
+  ground_pointer = input_pointer = nonzero_entries = past_id = 0
+  accuracies = [] # also store accuracy in an array so it can get passed + sent to HTML
+
+  # Initialization of object
+  ground_phonemes = []
+  for id in ground_ids:
+    phoneme_obj = PhonemeWrapper(processor.decode(id), id, None, None)
+    ground_phonemes.append(phoneme_obj)
+
+  while ground_pointer < len(ground_phonemes) and input_pointer < len(input_ids):
+
+    # If match is found, we are done. Move to next ground_id
+    if ground_phonemes[ground_pointer].id == input_ids[input_pointer]:
+      # if abs(ground_pointer - input_pointer) < 4: # ids should be close to each other
+      # if nonzero_entries < 8: # ids should be close to each other
+
+      ground_phonemes[ground_pointer].corresponding_id = past_id = input_pointer
+
+      # Probability analysis
+      sorted_probs = sorted(logits[0][input_pointer].tolist(), reverse=True)
+      next_five_avg = sum(sorted_probs[1:6])/5
+      ground_phonemes[ground_pointer].accuracy = round((1 - (next_five_avg / sorted_probs[0]))*100, 2)
+      accuracies.append(ground_phonemes[ground_pointer].accuracy)
+
+      print("Next five: ", next_five_avg, " , ", " High ", sorted_probs[0])
+
+      # Next step
+      ground_pointer += 1
+      nonzero_entries = 0
+
+
+    # If match is still not found by end of string, mark as unfound. "reset" input_pointer to prev char
+    elif input_pointer == len(input_ids)-1:
+      # print("Ground pointer is: ", ground_pointer ," and corresponding phoneme is ", ground_ids[ground_pointer])
+      input_pointer = past_id
+      ground_phonemes[ground_pointer].corresponding_id = -1
+      ground_phonemes[ground_pointer].accuracy = 0
+      accuracies.append(0)
+      ground_pointer += 1
+
+    # If there is a nonzero entry that isn't a match, record it
+    elif input_ids[input_pointer] not in [211, 212, 213]:
+      nonzero_entries += 1
+    
+    # Input_pointer still updates
+    input_pointer += 1
+
+  return ground_phonemes, accuracies
+
+print("Starting")
+# Render form
+@APP.route('/')
+def index():
+  print("eeeeeeef")
+  return flask.render_template('index.html')
+
+
+@APP.route('/result', methods = ["POST", "GET"])
+def result():
+  print('hello buddy')
+  return flask.render_template('index.html', name = "ffffff")
+
+
+
+@APP.route('/handle_post', methods=['POST', 'GET'])
+def handle_post():
+    """ Displays the index page accessible at '/'
+    """
+    # Record audio
+    freq = 16000
+    duration = 3
+    recording = sd.rec(int(duration * freq), samplerate=freq, channels=1)
+    sd.wait()
+
+    # IDs of input recording
+    recording = np.reshape(recording, -1)
+    input_dict = processor(recording, return_tensors="pt", padding=True, sampling_rate=16000)
+    logits = model(input_dict.input_values).logits
+    pred_ids = torch.argmax(logits, dim=-1)[0]
+    input_ids = pred_ids.tolist()
+
+    # IDs of ground truth recording
+    ground_phonemes = "nei5|ɡiuɜ|me1|minɡ4|aa1"
+    with processor.as_target_processor():
+        ground_ids = processor(ground_phonemes).input_ids
+
+    print("Ground ids: ", ground_ids)
+    print("Input ids: ", input_ids)
+
+    # Mapping ground truth –> input + Probability analysis
+    analysis, accuracies = probability_analysis(ground_ids, input_ids, logits)
+
+    print("Accuracies: ", accuracies)
+
+    # Getting input phonemes
+    input_ids_new = []
+    for i in input_ids:
+      input_ids_new.append(i)
+      input_ids_new.append(211) # space in between every phoneme
+
+    input_phonemes = processor.decode(input_ids_new)
+
+    for phoneme in analysis:
+        phoneme.print_all()
+
+    # Putting in appropriate format for HTML
+    ground_phonemes = "n ei5 ɡ iuɜ m e1 m inɡ4 aa1"
+    input_phonemes = input_phonemes.replace("|", " ")
+
+    bob = "<span style='color: #00ccff;'>Hello world</span>"
+
+    # Results
+    results = '''
+                hey
+              '''
+
+    #return flask.render_template('index.html', ground_phonemes = ground_phonemes, input_phonemes = input_phonemes, results = results)
+
+    return flask.render_template('index.html', ground_phonemes = ground_phonemes, input_phonemes = input_phonemes, analysis = analysis)
+
+
+if __name__ == '__main__':
+    APP.debug=True
+    APP.run(debug=True)
+
